b0ca27ac7dc8ab7978e6ea3311f7961d74275225
Samad126 <samed192005@gmail.com>
2025-05-22 21:47:23 +0400

    feat: implement AirportIndex component to display a list of airports in a table format

--- src/pages/Airports/Index.tsx
+++ src/pages/Airports/Index.tsx
@@ -1,7 +1,51 @@
-function AirportIndex() {
-  return (
-    <div>AirportIndex</div>
-  )
-}
+import {
+  Box,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
 
-export default AirportIndex
+const airports = [
+  {
+    id: 1,
+    name: "Heathrow Airport",
+    city: "London",
+  },
+  // Add more airport entries here if needed
+];
+
+export default function AirportIndex() {
+  return (
+    <Box>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Airports
+      </Typography>
+
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>City</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {airports.map((airport) => (
+              <TableRow key={airport.id}>
+                <TableCell>{airport.id}</TableCell>
+                <TableCell>{airport.name}</TableCell>
+                <TableCell>{airport.city}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+}
