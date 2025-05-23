270dc32a95f0dffb99091611b8415bfae7b2ab00
Samad126 <samed192005@gmail.com>
2025-05-23 19:43:49 +0400

    feat: migrate routing logic from util to route directory for improved structure and organization

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 
 import { RouterProvider } from "react-router";
-import { router } from "./util/route";
+import { router } from "./route/route";
 import AppContextProvider from "./context/AppContextProvider";
 
 createRoot(document.getElementById("root")!).render(
